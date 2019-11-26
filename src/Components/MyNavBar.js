d4a080711e7d5f24e471f742ffb0ecd1b640a77d
bluntjay <ialexjoshua@gmail.com>
2019-11-26 15:53:24 +0200

    Updated CSS & added new Projects

--- src/Components/MyNavBar.js
+++ src/Components/MyNavBar.js
@@ -7,34 +7,23 @@ export default class MyNavBar extends React.Component {
     render() {
         return (
             <div className="naviWrap">
-<<<<<<< HEAD
+
             <div className="navi">
                     <ul className="listStyle" >
         <li className="naviList"><a href="#about">About</a></li>
-=======
 			<div className="navi">
     				<ul className="listStyle" >
         <li className="naviList"><a href="#about">About Me</a></li>
->>>>>>> d576d7f0f417821b950ce3558d0b3c0de2635871
         <li className="naviList"><a href="#portfolio">Portfolio</a></li>
         <li className="naviList"><a href="#myProfile">Profile</a></li>
         <li className="naviList"><a href="#skills">Skills</a></li>
         <li className="naviList"><a href="#contact">Contact</a></li>
-<<<<<<< HEAD
         </ul>
-
-
         </div>
-            </div>
-    
-=======
 		</ul>
-
-
     	</div>
         	</div>
-	
->>>>>>> d576d7f0f417821b950ce3558d0b3c0de2635871
+
         )
 
     }
