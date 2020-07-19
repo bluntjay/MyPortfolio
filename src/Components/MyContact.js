ebf995b5afcb0f493c072f148d22e12a944d35cc
Alex <ialexjoshua@gmail.com>
2020-07-19 12:59:10 +0200

    Changed contact links

--- src/Components/MyContact.js
+++ src/Components/MyContact.js
@@ -1,49 +1,48 @@
 import React from 'react';
 
+const ContactLink = props => (
+	<li className="contactButton">
+		<a className="contactLinks" target="_target" href={props.urlLink}>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span className={props.urlIcon}></span>
+		</a>
+	</li>
+);
 
-export default class MyContact extends React.Component{
-	render(){
-		return(
-			<div id="footerStyle"  >
-			<footer id="contact">
-		<h2 className="contactHead">Contact Me</h2>
-	<ul id="footerList">
-		<div className="links">
-	
-		<a className="contactLinks" target="_target" href="https://www.linkedin.com/in/alex-joshua-80817018b/">
-		<i className="fab fa-linkedin-in fa-2x"></i></a>
-
-		<a className="contactLinks" target="_target" href="https://github.com/hyperxtend">
-		<i className="fab fa-github fa-2x"></i></a>
-
-		
-		<a className="contactLinks" target="_target" href="https://twitter.com/hyperxtend">
-		<i className="fab fa-twitter fa-2x"></i></a>
-
-		
-		<a className="contactLinks" target="_target" href="https://aboutme.google.com">
-		<i className="fab fa-google-plus-g fa-2x"></i></a>
-
-		<a className="contactLinks" target="_target" href="https://www.behance.net/alexjoshua1">
-		<i class="fab fa-behance"></i></a>
-
-		
-		<a className="contactLinks" target="_target" href="https://www.tumblr.com/blog/aliosbourne">
-		<i className="fab fa-tumblr fa-2x"></i></a>
-
-		<a className="contactLinks" target="_target" href="https://www.instagram.com/ialexjoshua/">
-		<i className="fab fa-instagram fa-2x"></i></a>
-
-
-		</div>
-	</ul>
-
-	</footer>
+const MyContact = () => (
+	<div className="contactContainer">
+		<footer id="contact">
+			<ul id="footerList">
+				<ContactLink
+					urlLink="https://www.linkedin.com/in/alex-joshua-80817018b/"
+					urlIcon="fab fa-linkedin-in fa-3x"
+				/>
+				<ContactLink
+					urlLink="https://github.com/hyperxtend"
+					urlIcon="fab fa-github fa-3x"
+				/>
+				<ContactLink
+					urlLink="https://twitter.com/hyperxtend"
+					urlIcon="fab fa-twitter fa-3x"
+				/>
+				<ContactLink
+					urlLink="https://www.behance.net/alexjoshua1"
+					urlIcon="fab fa-behance"
+				/>
+				<ContactLink
+					urlLink="https://www.tumblr.com/blog/aliosbourne"
+					urlIcon="fab fa-tumblr fa-3x"
+				/>
+				<ContactLink
+					urlLink="https://www.instagram.com/ialexjoshua/"
+					urlIcon="fab fa-instagram fa-3x"
+				/>
+			</ul>
+		</footer>
 	</div>
+);
 
-
-
-			)
-	}
-}
-
+export default MyContact;
